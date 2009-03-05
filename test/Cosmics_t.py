c8c47e3dc7a17d82bfb528dcc09263faa46c3616
Dave Evans <sha1-0a127cb30770fd84c4fd981547c09a2be8db1484@cern.ch>
2009-03-05 15:40:33 +0000

    Initial import of DataProcessing package with prototype implementation for a data taking scenario object that provides the configuration interface classes for standard data processing configurations such as prompt reco, alca, dqm etc

---
+++ test/Cosmics_t.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+"""
+_Cosmics_
+
+Test for Cosmics Scenario implementation
+
+"""
+
+
+import unittest
+import FWCore.ParameterSet.Config as cms
+from Configuration.DataProcessing.GetScenario import getScenario
+
+
+
+class CosmicsScenarioTest(unittest.TestCase):
+    """
+    unittest for cosmics scenario
+
+    """
+
+    def testA(self):
+        """get the scenario"""
+
+        try:
+            scenario = getScenario("Cosmics")
+        except Exception, ex:
+            msg = "Failed to get Cosmics scenario\n"
+            msg += str(ex)
+            self.fail(msg)
+
+
+    def testPromptReco(self):
+        """test promptReco method"""
+
+        proc = cms.Process("RECO")
+        proc.writeReco = cms.OutputModule("writeReco")
+        
+        
+        scenario = getScenario("Cosmics")
+        try:
+            scenario.promptReco(proc, proc.writeReco)
+        except Exception, ex:
+            msg = "Failed to create Prompt Reco configuration\n"
+            msg += "for Cosmics Scenario\n"
+            msg += str(ex)
+            self.fail(msg)
+
+    def testDQMHarvesting(self):
+        """test dqmHarvesting  method"""
+
+
+        scenario = getScenario("Cosmics")
+
+        try:
+            scenario.dqmHarvesting("dataset", 123456, "GLOBALTAG::ALL",
+                                   "file1", "file2", "file3")
+        except Exception, ex:
+            msg = "Failed creating DQM Harvesting configuration "
+            msg += "for Cosmics scenario:\n"
+            msg += str(ex)
+            self.fail(msg)
+
+            
+    def testAlcaReco(self):
+        """ test alcaReco method"""
+        scenario = getScenario("Cosmics")
+
+        proc = cms.Process("ALCARECO")
+
+        try:
+            scenario.alcaReco(proc,
+                              "ALCARECOStreamHcalCalHOCosmics",
+                              "ALCARECOStreamTkAlCosmics")
+        except Exception, ex:
+            msg = "Error preparing Alca Reco configuration\n"
+            msg += str(ex)
+            self.fail(msg)
+                              
+
+    
+if __name__ == '__main__':
+    unittest.main()
